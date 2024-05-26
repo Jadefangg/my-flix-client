bc780aaae3f66ea51696e41845983955995b48fe
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-05-26 17:49:34 +0530

    Used hook - Removed movies added API to fetch for search
    3.4

--- dist/src/components/main/main-view/main-view.jsx
+++ dist/src/components/main/main-view/main-view.jsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [books, setBooks] = useState([
-    {
-      id: 1,
-      title: "Inglorious Bastards",
-      image:
-        "https://m.media-amazon.com/images/I/81KrCOVgp1L._AC_UY327_FMwebp_QL65_.jpg",
-      author: "Martin Scorsese"
-    },
-    {
-      id: 2,
-      title: "The Dark Knight Rises",
-      image:
-        "https://m.media-amazon.com/images/I/81DqqPSKoeL._AC_UY327_FMwebp_QL65_.jpg",
-      author: "Christopher Nolan"
-    },
-    {
-      id: 3,
-      title: "The Good The Bad and The Ugly",
-      image:
-        "https://m.media-amazon.com/images/I/91BDDsP0clL._AC_UY327_FMwebp_QL65_.jpg",
-      director: "Sergio Leone"
-    },
-   
-  ]);
+  const [books, setBooks] = useState([]);
+  useEffect(() => {fetch("https://openlibrary.org/search.json?q=star+wars");
+}, []);
 
   const [selectedBook, setSelectedBook] = useState(null); //Flag to show the selected book.
   if (selectedBook) {
