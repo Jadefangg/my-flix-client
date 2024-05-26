2c7542db18c5e76214e21f2d84fb86427c9daed5
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-05-26 17:57:09 +0530

    Added promise and hook import. statement.
    3.4

--- dist/src/components/main/main-view/main-view.jsx
+++ dist/src/components/main/main-view/main-view.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { useState,useEffect } from "react"; //HOOKS
 
 export const MainView = () => {
   const [books, setBooks] = useState([]);
-  useEffect(() => {fetch("https://openlibrary.org/search.json?q=star+wars");
+  useEffect(() => {fetch("https://openlibrary.org/search.json?q=star+wars")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("books from api:", data);
+  });
+
 }, []);
 
   const [selectedBook, setSelectedBook] = useState(null); //Flag to show the selected book.
