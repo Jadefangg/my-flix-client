4dbd608fe4bd63cdb701fc67a15dc3ca6e76d780
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-05-29 14:53:13 +0530

    Login-view update.
    3.5

--- dist/src/components/main/login-view/login-view.jsx
+++ dist/src/components/main/login-view/login-view.jsx
@@ -1,14 +1,34 @@
-export const LoginView=() =>
-{
-return(
-    <form>
-        <label>
-            Username:
-            <input type="text" />
-            Password:
-            <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
+import React from "react";
+
+export const LoginView = () => {
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://openlibrary.org/account/login.json", {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <button type="submit">
+        Submit
+      </button>
     </form>
-   )
-}
+  );
+};
