3bfdd2229cbf74ab598e92ca0ed78584a3ef64ae
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-05-29 23:58:34 +0530

    Updated fetch and the Username and Password input fields along with onLoggedIn
    3.5

--- dist/src/components/main/login-view/login-view.jsx
+++ dist/src/components/main/login-view/login-view.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-export const LoginView = () => {
-  const handleSubmit = (event) => {
+export const LoginView = ({ onLoggedIn }) => {
+const [username, setUsername] = useState("");
+const handleSubmit = (event) => {
     // this prevents the default behavior of the form which is to reload the entire page
     event.preventDefault();
 
@@ -11,20 +12,32 @@ export const LoginView = () => {
     };
 
     fetch("https://openlibrary.org/account/login.json", {
-      method: "POST",
-      body: JSON.stringify(data)
-    });
-  };
-
-  return (
+        method: "POST",
+        body: JSON.stringify(data)
+      }).then((response) => {
+        if (response.ok) {
+          onLoggedIn(username);
+        } else {
+          alert("Login failed");
+        }
+      });
+  return ( //Tells the login api to validate username and password - callback!
     <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type="text" />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} // this updates the state of the username variable
+        />
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} // this updates the state of the username variable
+        />
       </label>
       <button type="submit">
         Submit
