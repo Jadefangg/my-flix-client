fe392fac09f106f2755c5d92ee1cae7b04faae57
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-06-05 19:59:46 +0530

    Bootstrap container imported.
    <Mainview> wrapped in <Container> in the return statement as well.

--- dist/src/index.jsx
+++ dist/src/index.jsx
@@ -1,11 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap/lib/Tab"; //Bootstrap components.
+import "./index.scss";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 
 import "./index.scss";
 
 const App = () => {
- return <MainView />;
-};
+    return (
+        <Container>
+          <MainView />
+        </Container>
+      )};
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
