00308a0d0ab74cd5618512c537b0c4811f4d51b5
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-06-14 13:59:22 +0530

    Final update for 3.7.
    Imported movie-card into profile-view so the favorite movies list can be rendered in profile view. Profile-view now contains user info along with favorite movies which was given as a button and a handle function was provided to handle addition of movies to favorites. .

--- dist/src/components/main/profile-view/profile-view.jsx
+++ dist/src/components/main/profile-view/profile-view.jsx
@@ -1,13 +1,14 @@
+import { MovieCard } from './movie-card';
 import React, { useState, useEffect } from "react";
 import { UserInfo } from "./user-info";
 import { Link } from "react-router-dom";
-import { UserUpdate } from "./user-update";
-import { UserDelete } from "./user-delete";
-import { FavoriteMovies } from "./favorite-movie";
+//import { UserUpdate } from "./user-update";
+//import { UserDelete } from "./user-delete";
+//import { FavoriteMovies } from "./favorite-movie";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./profile-view.scss";
 
-export const ProfileView = ({ movies, user }) => {//Displaying user info via /users endpoint.
+export const ProfileView = ({ movie, user }) => {//Displaying user info via /users endpoint.
     const [userInfo, setUserInfo] = useState(null);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -49,7 +50,9 @@ export const ProfileView = ({ movies, user }) => {//Displaying user info via /us
       <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="New email" />
       <button onClick={handleUpdate}>Update</button>
         {/* ...other components... */}
+      <MovieCard movie={movie} /> 
       </div>
+      
     );
   };
 
