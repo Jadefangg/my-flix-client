8f777214b5879db1c179f7f5d4d8948d0abb4db1
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-06-22 14:03:22 +0530

    3.9 final updates
    3.9

--- dist/src/components/main/movie-card/movie-card.jsx
+++ dist/src/components/main/movie-card/movie-card.jsx
@@ -1,32 +1,33 @@
-import React from "react"; //React library. 
-import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap"; //Bootstrap components.
-import { Link } from "react-router-dom"; //React Router component.
-import "./movie-card.scss"; //Styling for the movie card.
+import PropTypes from 'prop-types';
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, AddToFavoritesClick }) => {
-  const handleClickToFavorites =() =>{ //Function to add movie to favorites.
-    AddToFavoritesClick(movie._id);
-  }
-    return (//Card component from Bootstrap.
-      <Card>
-      <Card.Img variant="top" src={movie.image} />
-      <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.author}</Card.Text>
-        <Link to = {`/movies/${encodeURIComponent(movie._id)}`}> 
-        <Button variant="link">Open</Button>
-        </Link>
-        <Button variant="link" onClick={handleClickToFavorites}>Add to your favs!</Button>
-        </Card.Body>
-    </Card>
-  );
+export const MovieCard = ({ movie }) => {
+    return (
+        <Card className='movie-card h-100'>
+            <div className="ratio ratio-16x9">
+                <Card.Img variant="top" src={movie.ImagePath} />
+            </div>
+            <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>{movie.Director.Name}</Card.Text>
+                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                    <Button variant="outline-light">Open</Button>
+                </Link>
+            </Card.Body>
+        </Card>
+    );
 };
-  MovieCard.propTypes = {
+
+MovieCard.propTypes = {
     movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired,
-      author: PropTypes.string,
+        Title: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        }).isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        }).isRequired,
     }).isRequired,
-    AddToFavoritesClick: PropTypes.func.isRequired, //cannot be insde movie: PropTypes because it is moviecards' Prop!
-  };
+};
